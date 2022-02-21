0c27f87728f4eb43318c9f3fed87f96e66a95e32
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-21 16:09:13 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,7 @@
+const person = {name:'shuvo',Id:1610,Address:'Uttara',Phone:{Number1:'01772385111',Number2:'01772385111444'}
+}
+
+const{name,ID,Address,Phone}=person;
+const{Number1,Number2}=person.Phone;
+
+console.log(Number1,Number2);
