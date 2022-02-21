b902b23b05d1af3f95f6ad5b3fb65bee0b7512fa
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-02-21 16:45:42 +0600

    nested-object.js

---
+++ nested-object.js
@@ -0,0 +1,13 @@
+const company = {
+    name:'Expression',Id:1610,
+    ceo:{name:'sonjoy',Id:2211,Address:'uttara'},
+    web:{work:'developer',employe:22,farmwork:'js',
+    tack:{
+        first:'html',
+        second:'css',
+        third:'js'
+    }
+}
+}
+console.log(company?.ceo?.Address);
+console.log(company?.web?.tack?.third);
